70e790d0e5a9ed0c222fc08de295202b3154efab
Piotr Francug <piotr.francug@gmail.com>
2019-10-11 09:22:34 +0200

    Styles moved to their own folder

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const createElement = (link) => {
+const showPreview = (link) => {
     const previewEl = document.createElement('div')
     previewEl.setAttribute('id', 'preview')
-    previewEl.style.position = 'fixed'
-    previewEl.style.left = '400px'
-    previewEl.style.top = '25px'
-    previewEl.style['height'] = 'calc(100% - 50px)'
-    previewEl.style['width'] = 'calc(100% - 425px)'
 
     const imageEl = document.createElement('img')
     imageEl.setAttribute('src', link)
-    imageEl.style['max-height'] = '100%'
-    imageEl.style['max-width'] = '100%'
 
     previewEl.appendChild(imageEl)
     document.querySelector('body').appendChild(previewEl)
 }
 
-const removePreview = () => {
+const hidePreview = () => {
     const elements = document.querySelectorAll('#preview')
-    elements.forEach(element => {
-        element.parentNode.removeChild(element)
-    })
+    elements.forEach(element => element.parentNode.removeChild(element))
 }
 
 const images = document.querySelectorAll('.fileThumb')
@@ -29,9 +20,7 @@ images.forEach((image) => {
     image.addEventListener('mouseover', (e) => {
         const anchor = e.target.closest("a").getAttribute('href')
         const link = anchor.replace(/^/, 'http:')
-        createElement(link)
+        showPreview(link)
     })
-    image.addEventListener('mouseout', () => {
-        removePreview()
-    })
+    image.addEventListener('mouseout', () => hidePreview())
 })
